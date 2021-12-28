2a1cbaee4cbb7f6b5b346626b47499ba402540f5
Mateen Ulhaq <mulhaq2005@gmail.com>
2021-12-28 04:51:06 -0800

    feat: complete simple example

---
+++ examples/simple/src/main.py
@@ -0,0 +1,41 @@
+from argparse import ArgumentParser
+from time import sleep
+
+import tomlkit as toml
+
+
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument("--continue", dest="continue_", action="store_true")
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+
+    with open("../assets/config.toml") as f:
+        config = toml.loads(f.read())
+
+    if args.continue_:
+        with open("../results/state_dict.toml") as f:
+            state_dict = toml.loads(f.read())
+    else:
+        state_dict = {"epoch": 0}
+
+    max_epochs = config["epochs"]
+    start_epoch = state_dict["epoch"]
+    end_epoch = min(max_epochs, start_epoch + 10)
+
+    for epoch in range(start_epoch, end_epoch):
+        print(f"Epoch: {epoch}", flush=True)
+        state_dict["epoch"] = epoch + 1
+
+        with open("../results/state_dict.toml", "w") as f:
+            f.write(toml.dumps(state_dict))
+
+        sleep(5)
+
+
+if __name__ == "__main__":
+    main()
--- examples/simple/submit_job.py
+++ examples/simple/submit_job.py
@@ -2,37 +2,31 @@ import easy_slurm as ezs
 
 
 ezs.submit_job(
-    job_root="$HOME/.local/share/easy_slurm",
-    src="./test_src",
-    assets="./test_assets",
-    dataset="./test_data.tar.gz",
+    job_root="$HOME/.local/share/easy_slurm/example-simple",
+    src="./src",
+    assets="./assets",
+    dataset="./data.tar.gz",
     on_run="python main.py",
     on_continue="python main.py --continue",
     setup="""
-        git clone --depth 1 https://github.com/InterDigitalInc/CompressAI "$SLURM_TMPDIR/compressai"
         module load python/3.9
         virtualenv --no-download "$SLURM_TMPDIR/env"
         source "$SLURM_TMPDIR/env/bin/activate"
         pip install --no-index --upgrade pip
-        pip install --no-index -r requirements.txt
-        pip install -e "$SLURM_TMPDIR/compressai"
+        pip install --no-index -r "$SLURM_TMPDIR/src/requirements.txt"
     """,
     teardown="""
-        mv "$SLURM_TMPDIR/src/logs" "$SLURM_TMPDIR/results/"
     """,
     setup_continue="""
         setup
-        cd "$SLURM_TMPDIR"
-        mv results/logs src/
     """,
     sbatch_options={
-        "job-name": "example",
+        "job-name": "example-simple",
         "account": "def-ibajic",
-        "time": "3:00:00",
-        "gres": "gpu:1",
+        "time": "0:03:00",
         "nodes": "1",
         "ntasks-per-node": "1",
-        "cpus-per-task": "6",
-        "mem": "32000M",
+        "cpus-per-task": "1",
+        "mem": "4000M",
     },
 )
