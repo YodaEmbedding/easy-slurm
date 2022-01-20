fdea214ed9d6498f92920d09c054dd020b405f08
Mateen Ulhaq <mulhaq2005@gmail.com>
2022-01-19 23:02:38 -0800

    chore: bump version number

--- easy_slurm/__init__.py
+++ easy_slurm/__init__.py
@@ -35,7 +35,7 @@ If the current run successfully completes, `status` ends with
 `interrupting` and ends with `incomplete`.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 from .jobs import (
     create_job_dir,
