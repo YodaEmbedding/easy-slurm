3987d8d4078e221e13b4ec80b8f7c8930f5229d6
Mateen Ulhaq <mulhaq2005@gmail.com>
2023-02-25 23:35:24 -0800

    style: misc

--- easy_slurm/jobs.py
+++ easy_slurm/jobs.py
@@ -88,32 +88,33 @@ def submit_job(
         Path to the newly created job directory.
     """
     job_name = sbatch_options.get("job-name", "untitled")
-    job_dir = format_with_config(job_dir, {"job_name": job_name})
-    job_dir = _expand_path(job_dir)
+    job_dir = _expand_path(format_with_config(job_dir, {"job_name": job_name}))
     create_job_dir(job_dir, src, assets)
 
-    job_path = f"{job_dir}/job.sh"
-    job_script_str = create_job_script_source(
-        sbatch_options=sbatch_options,
-        on_run=on_run,
-        on_run_resume=on_run_resume,
-        setup=setup,
-        setup_resume=setup_resume,
-        teardown=teardown,
-        job_dir=job_dir,
-        cleanup_seconds=cleanup_seconds,
-        resubmit_limit=resubmit_limit,
+    _write_script(
+        filename=f"{job_dir}/job.sh",
+        text=create_job_script_source(
+            sbatch_options=sbatch_options,
+            on_run=on_run,
+            on_run_resume=on_run_resume,
+            setup=setup,
+            setup_resume=setup_resume,
+            teardown=teardown,
+            job_dir=job_dir,
+            cleanup_seconds=cleanup_seconds,
+            resubmit_limit=resubmit_limit,
+        ),
     )
-    _write_script(job_path, job_script_str)
 
-    job_interactive_path = f"{job_dir}/job_interactive.sh"
-    job_interactive_script_str = create_job_interactive_script_source(
-        sbatch_options=sbatch_options,
-        job_path=job_path,
-        job_dir=job_dir,
-        cleanup_seconds=cleanup_seconds,
+    _write_script(
+        filename=f"{job_dir}/job_interactive.sh",
+        text=create_job_interactive_script_source(
+            sbatch_options=sbatch_options,
+            job_path=f"{job_dir}/job.sh",
+            job_dir=job_dir,
+            cleanup_seconds=cleanup_seconds,
+        ),
     )
-    _write_script(job_interactive_path, job_interactive_script_str)
 
     if submit:
         submit_job_dir(job_dir, interactive)
@@ -134,33 +135,22 @@ def create_job_script_source(
 ) -> str:
     """Returns source for job script."""
     job_dir = _expand_path(job_dir)
-
-    vars_str = VARS_TEMPLATE.format(
-        easy_slurm_version=__version__,
-        job_dir=job_dir,
-        resubmit_limit=resubmit_limit,
-    )
-
-    setup = _fix_indent(setup, 1)
-    setup_resume = _fix_indent(setup_resume, 1)
-    teardown = _fix_indent(teardown, 1)
-
     fix_quotes = lambda x: _quote_single_quotes(x.strip())
-    on_run = fix_quotes(on_run)
-    on_run_resume = fix_quotes(on_run_resume)
-
-    sbatch_options_str = _sbatch_options_to_str(
-        sbatch_options, job_dir, cleanup_seconds
-    )
 
     return JOB_SCRIPT_TEMPLATE.format(
-        sbatch_options_str=sbatch_options_str,
-        vars_str=vars_str,
-        on_run=on_run,
-        on_run_resume=on_run_resume,
-        setup=setup,
-        setup_resume=setup_resume,
-        teardown=teardown,
+        sbatch_options_str=_sbatch_options_to_str(
+            sbatch_options, job_dir, cleanup_seconds
+        ),
+        vars_str=VARS_TEMPLATE.format(
+            easy_slurm_version=__version__,
+            job_dir=job_dir,
+            resubmit_limit=resubmit_limit,
+        ),
+        on_run=fix_quotes(on_run),
+        on_run_resume=fix_quotes(on_run_resume),
+        setup=_fix_indent(setup, 1),
+        setup_resume=_fix_indent(setup_resume, 1),
+        teardown=_fix_indent(teardown, 1),
     )
 
 
@@ -170,16 +160,14 @@ def create_job_interactive_script_source(
     job_path: str,
     cleanup_seconds: int,
 ) -> str:
-    """Returns source for job script."""
+    """Returns source for interactive job script."""
     job_dir = _expand_path(job_dir)
     job_path = _expand_path(job_path)
 
-    sbatch_options_str = _sbatch_options_to_str(
-        sbatch_options, job_dir, cleanup_seconds
-    )
-
     return JOB_INTERACTIVE_TEMPLATE.format(
-        sbatch_options_str=sbatch_options_str,
+        sbatch_options_str=_sbatch_options_to_str(
+            sbatch_options, job_dir, cleanup_seconds
+        ),
         job_path=job_path,
     )
 
@@ -214,25 +202,19 @@ def submit_job_dir(job_dir: str, interactive: bool):
     """
     if interactive:
         job_interactive_path = f"{job_dir}/job_interactive.sh"
-        subprocess.run(
-            ["srun", "--pty", "bash", "--init-file", job_interactive_path],
-            check=True,
-            text=True,
-        )
-    else:
-        job_path = f"{job_dir}/job.sh"
-        result = subprocess.run(
-            ["sbatch", job_path],
-            check=True,
-            capture_output=True,
-            text=True,
-        )
+        cmd = ["srun", "--pty", "bash", "--init-file", job_interactive_path]
+        subprocess.run(cmd, check=True, text=True)
+        return
 
-        m = re.match(r"^Submitted batch job (\d+)$", result.stdout)
-        job_id = int(m.group(1))
+    job_path = f"{job_dir}/job.sh"
+    cmd = ["sbatch", job_path]
+    result = subprocess.run(cmd, check=True, capture_output=True, text=True)
 
-        with open(f"{job_dir}/job_ids", "w") as f:
-            print(job_id, file=f)
+    m = re.match(r"^Submitted batch job (\d+)$", result.stdout)
+    job_id = int(m.group(1))
+
+    with open(f"{job_dir}/job_ids", "w") as f:
+        print(job_id, file=f)
 
 
 def _expand_path(path: str) -> str:
@@ -241,10 +223,8 @@ def _expand_path(path: str) -> str:
 
 def _create_tar_dir(src, dst, root_name):
     transform = rf"s/^\./{root_name}/"
-    subprocess.run(
-        ["tar", "czf", dst, "-C", src, ".", "--transform", transform],
-        check=True,
-    )
+    cmd = ["tar", "czf", dst, "-C", src, ".", "--transform", transform]
+    subprocess.run(cmd, check=True)
 
 
 def _write_script(filename: str, text: str):
@@ -258,9 +238,11 @@ def _write_script(filename: str, text: str):
 def _sbatch_options_to_str(
     sbatch_options: dict[str, Any], job_dir: str, cleanup_seconds: int
 ) -> str:
-    sbatch_options = dict(sbatch_options)
-    sbatch_options["output"] = f"{job_dir}/slurm_jobid%j_%x.out"
-    sbatch_options["signal"] = f"B:USR1@{cleanup_seconds}"
+    sbatch_options = {
+        **sbatch_options,
+        "output": f"{job_dir}/slurm_jobid%j_%x.out",
+        "signal": f"B:USR1@{cleanup_seconds}",  # send USR1 to Bash before job end time
+    }
     return "\n".join(f"#SBATCH --{k}={v}" for k, v in sbatch_options.items())
 
 
