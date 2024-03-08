ba32bef885f2e66aa52cb5b5ae3641887c408b60
Mateen Ulhaq <mulhaq2005@gmail.com>
2024-03-07 22:24:53 -0800

    fix: examples

--- examples/simple/submit_job.py
+++ examples/simple/submit_job.py
@@ -4,11 +4,16 @@ easy_slurm.submit_job(
     job_dir="$HOME/.local/share/easy_slurm/{date}-{job_name}",
     src=["./src", "./assets"],
     setup="""
+        # Setup the environment:
         module load python/3.9
         virtualenv --no-download "$SLURM_TMPDIR/env"
         source "$SLURM_TMPDIR/env/bin/activate"
         pip install --no-index --upgrade pip
         pip install --no-index -r "$SLURM_TMPDIR/src/requirements.txt"
+
+        # Create/link output results directory:
+        mkdir -p "$JOB_DIR/results"
+        ln -s "$JOB_DIR/results" "$SLURM_TMPDIR/results"
     """,
     setup_resume="""
         setup
